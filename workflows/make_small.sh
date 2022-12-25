19af7481b1a09689cc30b081905ebfee4d689b50
Phillip Berndt <phillip.berndt@googlemail.com>
2022-12-25 20:35:29 +0100

    Improve keeping aspect ratio
    Use ffmpeg/imagemagick options to keep aspect ratio for rotated videos/images

--- workflows/make_small.sh
+++ workflows/make_small.sh
@@ -43,9 +43,9 @@ find "$SOURCE_DIR" -regextype egrep -regex '.*.(jpg|JPG|mp4|MP4)' -print0 | whil
 	echo -n "$SRC -> $TARGET  .."
 
 	if [[ $SRC =~ [jJ][pP][gG]$ ]]; then
-		convert "$SRC" -resize 1900x -quality 70 "$TARGET"
+		convert "$SRC" -resize "1900x1900>" -quality 70 "$TARGET"
 	else
-		ffmpeg -i "$SRC" -strict experimental -s 1280x720 -crf 30 -loglevel error "$TARGET" </dev/null
+		ffmpeg -i "$SRC" -strict experimental -vf scale=w=1280:h=1280:force_original_aspect_ratio=decrease -crf 30 -loglevel error "$TARGET" </dev/null
 	fi
 	if ! [ -e "$TARGET" ]; then
 		echo -n "  Last effort: Copying manually.. "
